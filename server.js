9ed2d65f85bfcf1273b01d5ca8792c2b6d4b80d7
unknown <brice.mebarki@gmail.com>
2020-09-22 12:27:50 +0200

    ajout de l'environnement node

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const path = require('path');
+// const path = require('path');
 
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end(path.basename('index.js'));
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end(path.basename('index.js'));
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://localhost:${port}/`);
+// });
+
